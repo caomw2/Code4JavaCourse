d40151246e49fb0df42dcfef104698ea447dbed4
caomw2 <caomw@hfut.edu.cn>
2026-05-18 21:03:13 +0800

    Add files via upload
    add database

---
+++ src/com/cao/database/DBUtils.java
@@ -0,0 +1,9 @@
+package com.cao.database;
+
+public class DBUtils {
+
+	public DBUtils() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/com/cao/database/UseJDBC.java
@@ -0,0 +1,70 @@
+package com.cao.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class UseJDBC {
+	public static void main(String[] args) throws SQLException {
+		String driver = "com.mysql.cj.jdbc.Driver";// 数据库驱动类所对应的字符串
+		String URL = "jdbc:mysql://localhost:3306/miscoursedb?"
+				+ "serverTimezone=UTC&useUnicode=true&characterEncoding=utf-8";
+
+		// URL语法格式如下
+		// jdbc:mysql:是固定的写法，后面跟主机名localhost，3306是默认的MySQL端口号
+		// serverTimezone=UTC是指定时区时间为世界统一时间
+		// useUnicode=true是指是否使用Unicode字符集，赋值为true
+		// characterEncoding=utf-8是指定字符编码格式为UTF8
+		Connection conn = null;
+		Statement stmtStatement = null;
+		ResultSet resultSet = null;
+		// Connection接口代表Java程序和数据库的连接对象，只有获得该连接对象后，才能访问数据库，并操作数据表
+		try {
+
+			Class.forName(driver);// 加载MySQL数据库驱动
+
+		} catch (java.lang.ClassNotFoundException e) {// 如果找不到这个类，执行下面的异常处理
+			System.out.println("驱动程序配置未配置成功!!!");
+		}
+
+		try {
+			conn = DriverManager.getConnection(URL, "root", "668899");// 建立和数据库的连接，并返回表示连接的Connection对象
+
+			System.out.println("数据库连接成功!!!");
+
+			stmtStatement =conn.createStatement();
+			String sqlString ="select * from users";
+			//stmtStatement =conn.prepareStatement(sqlString);
+			
+			resultSet = stmtStatement.executeQuery(sqlString);
+			
+			
+			while(resultSet.next()) {
+                int id = resultSet.getInt("idusers");
+                String name = resultSet.getString("name");
+                int age = resultSet.getInt("age");
+                String sex = resultSet.getString("sex");
+                
+               System.out.printf("%d\t%s\t%d\t%s%n", id, name, age,sex);
+               // System.out.printf(id);
+               
+            }
+			
+
+		} catch (Exception e) {// 未连接成功，执行下面的异常处理
+
+			System.out.println("数据库连接失败!!!");
+			e.printStackTrace();
+
+		} finally {
+
+			// 关闭数据库链接
+             resultSet.close();
+			stmtStatement.close();
+			conn.close();
+			System.out.println("关闭数据库链接!!!");
+		}
+	}
+}
