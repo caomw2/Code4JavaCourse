1e07c9d8047a78bf1f32e2260cb3ecbe09c733a5
caomw2 <caomw@hfut.edu.cn>
2026-05-27 23:41:25 +0800

    使用If else

---
+++ src/com/cao/common/IfElseDemo.java
@@ -0,0 +1,36 @@
+/**
+ * if else使用例子
+ */
+package com.cao.common;
+
+/**
+ * 
+ */
+public class IfElseDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 5;
+		boolean bool = false;
+
+		if (a > 5) {
+			System.out.println("Hello.");
+
+		} else {
+			System.out.println("How are you !");
+		}
+
+		if (bool == true) {
+			System.out.println("Very nice !");
+
+		} else {
+			System.out.println("So cute.");
+		}
+
+	}
+
+}
