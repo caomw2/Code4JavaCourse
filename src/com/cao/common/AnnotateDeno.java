e313a1e99d8030c992153c5795e998b656145c86
caomw2 <caomw@hfut.edu.cn>
2026-05-27 23:40:07 +0800

    使用注释

---
+++ src/com/cao/common/AnnotateDeno.java
@@ -0,0 +1,32 @@
+/**
+ * Java语言中的注释，单行注释和多行注释
+ */
+package com.cao.common;
+
+/**
+ * 
+ */
+public class AnnotateDeno {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// 1.单行注释，请输出你的姓名
+		System.out.println("Mingwei Cao");
+
+		/*
+		 * 2.这是多行注释 这是多行注释
+		 */
+		System.out.println("这是多行注释。");
+
+		/**
+		 * 3.文档注释 文档注释
+		 */
+		System.out.println("文档注释。");
+
+	}
+
+}
